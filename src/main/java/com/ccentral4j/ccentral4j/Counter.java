a0485e60498b6ce98cd58c095b60e3ca2886071d
Santtu Järvi <santtu@unity3d.com>
2016-12-19 13:55:55 +0200

    First draft

---
+++ src/main/java/com/ccentral4j/ccentral4j/Counter.java
@@ -0,0 +1,31 @@
+package com.ccentral4j.ccentral4j;
+
+class Counter {
+  private int last;
+  private int current;
+  private long ts;
+
+  public Counter() {
+    last = 0;
+    current = 0;
+    ts = System.currentTimeMillis();
+  }
+
+  private void refresh() {
+    while (ts + 60_000 < System.currentTimeMillis()) {
+      last = current;
+      current = 0;
+      ts = ts + 60_000;
+    }
+  }
+
+  public void increment(int amount) {
+    refresh();
+    current += amount;
+  }
+
+  public int getValue() {
+    refresh();
+    return last;
+  }
+}
---
+++ src/main/java/com/ccentral4j/ccentral4j/SchemaItem.java
@@ -0,0 +1,27 @@
+package com.ccentral4j.ccentral4j;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SchemaItem {
+  public String key;
+  public String title;
+  public String description;
+  @JsonProperty(value = "default")
+  public String defaultValue;
+  public String type;
+  @JsonIgnore
+  public String configValue;
+
+  public SchemaItem(String key, String title, String description, String defaultValue) {
+    this.key = key;
+    this.title = title;
+    this.description = description;
+    this.defaultValue = defaultValue;
+    type = "string";
+    configValue = null;
+  }
+
+  public SchemaItem() {
+  }
+}
